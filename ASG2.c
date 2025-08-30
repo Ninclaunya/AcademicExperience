1326be53741d4660918ecb4820cf453114679d98
Ninclaunya <nindie0304@gmail.com>
2025-08-30 20:30:59 +0700

    Update ASG2.c

--- ASG2.c
+++ ASG2.c
@@ -4,4 +4,5 @@ int main(){
     printf("Write a simple program that prints your name and favorite programming language\n");
     printf("Name : Nindie Claudia Vanya\n");
     printf("Favorite Programming Language: Python, PHP, C");
+    return 0;
 }
